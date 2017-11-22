f4d88abe293f63a2d52218ed1e4abafd5cbdc2a2
wuyanping <1063218918@qq.com>
2017-11-22 15:18:41 +0800

    [add] 添加basiclayout.js和basiclayout.less

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,13 +1,9 @@
 import React, {Component} from 'react'
-import {Input, Button} from 'antd'
 
 class Index extends Component {
     render() {
         return (
-            <header className="header">
-                <Button>index</Button>
-                <Input placeholder="What needs to be done?" />
-            </header>
+        	
         )
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 // import route from './routes/router.js'
-import Index from './components/index.jsx'
-import Sider from './components/sider.jsx'
+import BasicLayout from './layouts/BasicLayout.js'
 // import {Provider} from 'react-redux' //用到redux时使用
 
 import '../style/css/main.css'
 
 ReactDOM.render(
-    <div>
-        <Index />,
-        <Sider />
-    </div>,
+    <BasicLayout />,
     document.getElementById('app')
 )
