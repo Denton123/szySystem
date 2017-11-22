92bd98adb444c897f0ce4921bbd978dcf9c8a855
sdt <15626204923@163.com>
2017-11-22 15:25:30 +0800

    [mod]

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import Index from 'COMPONENT/index'
-import Sider from 'COMPONENT/sider'
+// import Sider from 'COMPONENT/sider'
 
 import '../style/css/main.less'
 
 ReactDOM.render(
     <div>
-        <Index />,
-        <Sider />
+        <Index />
     </div>,
     document.getElementById('app')
 )
