2cb3227811b77e759d8a2d3b17c745c65ef64652
bwx9394 <298057325@qq.com>
2017-11-23 16:19:56 +0800

    [add]新增动态路由

--- src/App.js
+++ src/App.js
@@ -3,76 +3,120 @@ import {
     BrowserRouter as Router,
     Route,
     Link,
-    Switch
+    Switch,
+    Redirect
 } from 'react-router-dom'
 
 function getViews(model) {
     return require(`VIEWS/${model}`).default
 }
 
-function CheckWork() {
-    return (
-        <div>checkwork</div>
-    )
-}
-
-function DayLog() {
-    return (
-        <div>dayLog</div>
-    )
-}
-
 const routes = [
     {
-        path: '/',
+        path: '/home',
         exact: true,
         component: getViews('Home'),
+        name: 'home',
         routes: [
+            {
+                name: '首页',
+                path: '/default',
+                component: () => <div>默认页</div>
+            },
             {
                 name: '个人事务管理',
                 path: '/personalAffairs',
-                component: CheckWork,
                 routes: [
                     {
                         name: '考勤',
                         path: '/checkWork',
-                        component: CheckWork
+                        component: () => <div>checkwork</div>
                     },
                     {
                         name: '每日日志',
                         path: '/dayLog',
-                        component: DayLog
+                        component: () => <div>dayLog</div>
                     }
                 ]
+            },
+            {
+                name: '项目管理',
+                path: '/project',
+                routes: [
+                    {
+                        name: '项目信息',
+                        path: '/projectInfo',
+                        component: () => <div>projectInfo</div>
+                    },
+                    {
+                        name: '项目申请管理',
+                        path: '/projectApply',
+                        component: () => <div>projectApply</div>
+                    },
+                    {
+                        name: '分配管理',
+                        path: '/projectDistribute',
+                        component: () => <div>projectDistribute</div>
+                    },
+                    {
+                        name: '工作日志',
+                        path: '/workLog',
+                        component: () => <div>workLog</div>
+                    }
+                ]
+            },
+            {
+                name: '测试',
+                path: '/test',
+                component: () => <div>test</div>
+            },
+            {
+                name: '测试1',
+                path: '/test1',
+                component: () => <div>test1</div>
             }
         ]
     },
     {
         path: '/login',
+        name: 'login',
         component: getViews('Login')
     },
     {
         path: '/register',
+        name: 'register',
         component: getViews('Register')
     },
     {
+        path: '/*',
+        name: '404',
         component: getViews('NotMatch')
     }
 ]
 
+const auth = true
+
 const App = () => (
     <Router>
-        <Switch>
-            {routes.map((com, idx) => (
-                <Route
-                    key={idx}
-                    exact={com.exact}
-                    path={com.path}
-                    component={com.component}
-                    {...com}
-                />
-            ))}
-        </Switch>
+        <div>
+            <Switch>
+                <Route exact path="/" render={() => (
+                    auth ? (
+                        <Redirect to="/home" />
+                    ) : (
+                        <Redirect to="/login" />
+                    )
+                )} />
+                {routes.map((com, idx) => (
+                    <Route
+                        key={idx}
+                        path={com.path}
+                        render={props => (
+                            <com.component {...props} routes={com.routes} />
+                        )} />
+                ))}
+            </Switch>
+        </div>
     </Router>
 )
 
--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -7,8 +7,43 @@
 import styles from './BasicLayout.less'
 import React from 'react'
 import { Layout, Menu, Breadcrumb, Icon } from 'antd'
+
+import {
+    Link,
+    Route,
+    Switch,
+    Redirect
+} from 'react-router-dom'
+
 const { Header, Content, Footer, Sider } = Layout
 const SubMenu = Menu.SubMenu
+
+// 子路由
+function SubRoute({route, idx, match}) {
+    return (
+        <Switch>
+            {
+                route.routes.map((child, sn) => (
+                    <Route
+                        key={`${idx}-${sn}`}
+                        exact={child.exact}
+                        path={`${match.path}${route.path}${child.path}`}
+                        render={props => (
+                            <Content style={{ margin: '0 16px' }}>
+                                <Breadcrumb style={{ margin: '16px 0' }}>
+                                    <Breadcrumb.Item>{route.name}</Breadcrumb.Item>
+                                    <Breadcrumb.Item>{child.name}</Breadcrumb.Item>
+                                </Breadcrumb>
+                                <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                                    <child.component />
+                                </div>
+                            </Content>
+                        )} />
+                ))
+            }
+        </Switch>
+    )
+}
 
 class BasicLayout extends React.Component {
     state = {
@@ -19,6 +54,82 @@ class BasicLayout extends React.Component {
         this.setState({ collapsed })
     }
     render() {
+        const routes = this.props.routes
+        const history = this.props.history
+        const location = this.props.location
+        const match = this.props.match
+
+        const dynamicSider = (
+            <div>
+                <div className="logo" />
+                <Menu theme="dark" mode="inline">
+                    {routes.map((route, idx) => {
+                        if (route.routes) {
+                            return (
+                                <SubMenu
+                                    key={idx}
+                                    title={<span><Icon type="user" /><span>{route.name}</span></span>}
+                                >
+                                    {
+                                        route.routes.map((child, sn) => (
+                                            <Menu.Item key={`${idx}-${sn}`}>
+                                                <Link to={`${match.path}${route.path}${child.path}`}>{child.name}</Link>
+                                            </Menu.Item>
+                                        ))
+                                    }
+                                </SubMenu>
+                            )
+                        } else {
+                            return (
+                                <Menu.Item key={idx}>
+                                    <Icon type="pie-chart" />
+                                    <span><Link to={`${match.path}${route.path}`}>{route.name}</Link></span>
+                                </Menu.Item>
+                            )
+                        }
+                    })}
+                </Menu>
+            </div>
+        )
+
+        const dynamicLayout = (
+            <div>
+                <Header style={{ background: '#fff', padding: 0 }} />
+                <Route exact path={match.path} render={() => (
+                    <Redirect to={`${match.path}/default`} />
+                )} />
+                {
+                    routes.map((route, idx) => {
+                        if (route.routes) {
+                            return (
+                                <SubRoute key={idx} route={route} idx={idx} match={match} />
+                            )
+                        } else {
+                            return (
+                                <Route
+                                    key={idx}
+                                    exact
+                                    path={`${match.path}${route.path}`}
+                                    render={props => (
+                                        <Content style={{ margin: '0 16px' }}>
+                                            <Breadcrumb style={{ margin: '16px 0' }}>
+                                                <Breadcrumb.Item>{route.name}</Breadcrumb.Item>
+                                            </Breadcrumb>
+                                            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                                                <route.component />
+                                            </div>
+                                        </Content>
+                                    )} />
+                            )
+                        }
+                    })
+                }
+                <Footer style={{ textAlign: 'center' }}>
+                    szy公司系统 ©2017 Created by szy
+                </Footer>
+            </div>
+        )
+
         const sider = (
             <div>
                 <div className="logo" />
@@ -79,10 +190,10 @@ class BasicLayout extends React.Component {
                     collapsed={this.state.collapsed}
                     onCollapse={this.onCollapse}
                 >
-                    {sider}
+                    {dynamicSider}
                 </Sider>
                 <Layout>
-                    {layout}
+                    {dynamicLayout}
                 </Layout>
             </Layout>
         )
