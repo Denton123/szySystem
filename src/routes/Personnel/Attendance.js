93abbb2919f43c62658fd98db11d6d86d514ccc6
wuyanping <1063218918@qq.com>
2017-12-22 15:23:06 +0800

    [mod] 编辑考勤管理

--- src/routes/Personnel/Attendance.js
+++ src/routes/Personnel/Attendance.js
@@ -1,250 +1,175 @@
-// 合同管理
+import ReactDOM from 'react-dom'
 import React, {Component} from 'react'
-import { Icon, Button, Table, Avatar } from 'antd'
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+import { Layout, Breadcrumb, Icon, Button, Calendar, Badge, Spin } from 'antd'
 import {
     Link,
     Route,
     Switch,
     Redirect
 } from 'react-router-dom'
+import ReactQuill from 'react-quill'
 
-import BasicCondition from 'COMPONENTS/basic/BasicCondition'
-import BasicOperation from 'COMPONENTS/basic/BasicOperation'
+const { Content } = Layout
+moment.locale('zh-cn')
 
-import SearchInput from 'COMPONENTS/input/SearchInput'
-// import CustomDatePicker from 'COMPONENTS/date/CustomDatePicker'
-import CustomRangePicker from 'COMPONENTS/date/CustomRangePicker'
+class checkwork extends Component {
+    state = {
+        attendanceData: [],
+        calendarTime: null,
+        loading: false
+    }
+    componentDidMount() {
+        this.setState({
+            calendarTime: moment().format('YYYY-MM-DD'),
+            loading: true
+        }, () => {
+            var data = this.state.calendarTime
+            axios.get(`/user/cs?data=${data}`)
+                .then(res => {
+                    console.log('access_token ---- ')
+                    console.log(res)
+                    data = []
+                    if (res.data !== []) {
+                        res.data.forEach((arrItem) => {
+                            data = [...arrItem, ...data]
+                        })
+                    }
+                    this.setState({
+                        attendanceData: data,
+                        loading: false
+                    })
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        })
+        var time = moment()
+        // console.log('time: ' + time)
+        // console.log('date: ' + moment(time)['date']())
+        // console.log('toArray: ' + moment(time).toArray())
+        // console.log('toJSON: ' + moment(time).toJSON())
+        // console.log('toISOString: ' + moment(time).toISOString())
+        // console.log(moment(time).toObject())
+        // console.log('toString: ' + moment(time).toString())
+        // console.log('weeksInYear: ' + moment(time).weeksInYear())
+        // console.log('isoWeeksInYear: ' + moment(time).isoWeeksInYear())
+        // console.log('month: ' + moment(time).startOf('month').format("YYYY-MM-DD"))
+        // console.log('month: ' + moment(time).endOf('month').format("YYYY-MM-DD"))
+        // console.log('day: ' + moment(time).startOf('month').day())
+        // var to=moment(time).endOf('month').format("YYYY-MM-DD")+" 23:59:59"
+    }
+    getListData = (value) => {
+        let oneDayArr = []
+        this.state.attendanceData.forEach(item => {
+            if (moment(moment(value).format('YYYY-MM-DD')).isSame(moment(item.userCheckTime).format('YYYY-MM-DD'))) {
+                let tip = {type: '', content: ''}
+                switch (item.timeResult) {
+                    case 'Normal':
+                        tip = {type: 'success', content: '正常'}
+                        break
+                    case 'Early':
+                        tip = {type: 'warning', content: '早退'}
+                        break
+                    case 'Late':
+                        tip = {type: 'warning', content: '迟到'}
+                        break
+                    case 'SeriousLate':
+                        tip = {type: 'warning', content: '严重迟到'}
+                        break
+                    case 'NotSigned':
+                        tip = {type: 'warning', content: '未打卡'}
+                        break
+                }
+                oneDayArr.push({ type: tip.type, content: `${moment(item.userCheckTime).format('YY-MM-DD, h:mm')}${tip.content}` })
+            }
+        })
+        return oneDayArr
+    }
 
-// 引入工具方法
-import {isObject, isArray, valueToMoment} from 'UTILS/utils'
-import { ajax } from 'UTILS/ajax'
-
-const columns = [
-    {
-        title: '用户名',
-        dataIndex: 'name',
-        key: 'name',
-        width: 70
-    },
-    {
-        title: '姓名',
-        dataIndex: 'realname',
-        key: 'realname',
-        width: 70
-    },
-    {
-        title: '性别',
-        dataIndex: 'gender',
-        key: 'gender',
-        width: 50,
-        render: text => <span>{text === 'male' ? '男' : '女'}</span>
-    },
-    {
-        title: '邮箱',
-        dataIndex: 'email',
-        key: 'email'
-    },
-    {
-        title: '电话',
-        dataIndex: 'phone',
-        key: 'phone'
-    },
-    {
-        title: '出生日期',
-        dataIndex: 'birth_date',
-        key: 'birth_date'
-    },
-    {
-        title: '头像',
-        dataIndex: 'avatar',
-        key: 'avatar',
-        render: text => <Avatar src={text} />
-    },
-    {
-        title: '职位',
-        dataIndex: 'job',
-        key: 'job'
-    },
-    {
-        title: '入职日期',
-        dataIndex: 'entry_date',
-        key: 'entry_date'
-    },
-    {
-        title: '离职日期',
-        dataIndex: 'quit_date',
-        key: 'quit_date'
-    },
-    {
-        title: '用户类型',
-        dataIndex: 'type',
-        key: 'type',
-        render: text => <span>{text === '0' ? '最高级管理' : text === '1' ? '管理' : '普通用户'}</span>
-    },
-    {
-        title: '操作',
-        key: 'action',
-        width: 200,
-        render: (text, record) => (
-            <span>
-                <a href="javascript:;">编辑</a>
-                <span className="ant-divider" />
-                <a href="javascript:;">删除</a>
-            </span>
+    // 自定义渲染日期单元格，返回内容会被追加到单元格
+    dateCellRender = (value) => {
+        var listData = this.getListData(value)
+        return (
+            <ul className="events">
+                {
+                    listData.map(item => (
+                        <li key={item.content}>
+                            <Badge status={item.type} text={item.content} />
+                        </li>
+                    ))
+                }
+            </ul>
         )
     }
-]
 
-const rowSelection = {
-    onChange: (selectedRowKeys, selectedRows) => {
-        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows)
-    }
-}
-
-class Contract extends Component {
-    state = {
-        entryDateValue: null,
-        quitDateValue: null,
-        tableSetting: {
-            loading: true,
-            pagination: false,
-            dataSource: []
+    getMonthData = (value) => {
+        if (value.month() === 8) {
+            return 1394
         }
     }
 
-    componentDidMount() {
-        console.log('componentDidMount ---- ')
-        this.getData(1, true)
+    // 自定义渲染月单元格，返回内容会被追加到单元格
+    monthCellRender = (value) => {
+        const num = this.getMonthData(value)
+        return num ? (
+            <div className="notes-month">
+                <section>{num}</section>
+                <span>Backlog number</span>
+            </div>
+        ) : null
     }
 
-    getData = (page, first = false) => {
-        let data = {
-            params: {
-                page: page
-            }
-        }
+    // 日期面板变化回调
+    onPanelChange = (value) => {
         this.setState({
-            tableSetting: {
-                loading: true
-            }
-        })
-        axios.get('/api/user', data)
-            .then(res => {
-                console.log(res)
-                let paginationChange = {
-                    onChange: this.handlePageChange
-                }
-                let pagination = {
-                    defaultCurrent: res.data.currentPage,
-                    pageSize: res.data.pageSize,
-                    total: res.data.total
-                }
-                this.setState({
-                    tableSetting: {
-                        loading: false,
-                        pagination: first ? Object.assign(pagination, paginationChange) : pagination,
-                        dataSource: res.data.data
+            calendarTime: moment(value).format('YYYY-MM-DD'),
+            loading: true
+        }, () => {
+            var data = this.state.calendarTime
+            axios.get(`/user/cs?data=${data}`)
+                .then(res => {
+                    data = []
+                    if (res.data !== []) {
+                        res.data.forEach((arrItem) => {
+                            data = [...arrItem, ...data]
+                        })
                     }
+                    this.setState({
+                        attendanceData: data,
+                        loading: false
+                    })
                 })
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
-
-    handlePageChange = (page) => {
-        console.log(page)
-        this.getData(page)
-    }
-
-    onChange = (field, value) => {
-        this.setState({
-            [field]: value
+                .catch(err => {
+                    console.log(err)
+                })
         })
+        console.log('value: ' + moment(value).format('YYYY-MM-DD'))
     }
 
-    onEntryDateChange = (value, valueString) => {
-        this.onChange('entryDateValue', value)
-        console.log('entryDateValue', valueString)
+    onSelect = () => {
+        console.log(22)
     }
-
-    onQuitDateChange = (value, valueString) => {
-        this.onChange('quitDateValue', value)
-        console.log('quitDateValue', valueString)
-    }
-
-    onEntryDateOk = (value) => {
-        console.log('onEntryDateOk: ', value)
-    }
-
-    onQuitDateOk = (value) => {
-        console.log('onQuitDateOk: ', value)
-    }
-
     render() {
         const child = this.props.child
         const route = this.props.route
         const history = this.props.history
         const location = this.props.location
         const match = this.props.match
-        const state = this.state
 
-        const entryDate = {
-            value: state.entryDateValue,
-            format: 'YYYY-MM-DD',
-            onChange: this.onEntryDateChange,
-            onOk: this.onEntryDateOk
-        }
-
-        const quitDate = {
-            value: state.quitDateValue,
-            format: 'YYYY-MM-DD',
-            onChange: this.onQuitDateChange,
-            onOk: this.onQuitDateOk
-        }
-
-        const condition = [
-            {
-                name: '搜索',
-                component: () => <SearchInput />
-            },
-            {
-                name: '入职时间',
-                component: () => <CustomRangePicker setting={entryDate} />
-            },
-            {
-                name: '离职时间',
-                component: () => <CustomRangePicker setting={quitDate} />
-            }
-        ]
-        const operationBtn = [
-            () => <Button type="primary">新增</Button>,
-            () => <Button type="danger">删除</Button>
-        ]
-        // const tableSetting = {
-        //     bordered: false,     // Boolean
-        //     loading: false,      // Boolean
-        //     pagination: {
-        //         current: 1,
-        //         pageSize: 10,
-        //         total: 500
-        //     },                   // Boolean || Object
-        //     size: 'default',     // 'default', 'middle', 'small'
-        //     expandedRowRender: record => <p>{record.description}</p>, // react组件函数   不显示时设置为undefined
-        //     title: () => 'Here is title',    // react组件函数    不显示时设置为undefined
-        //     showHeader: true,                // Boolean   表行头
-        //     footer: () => 'Here is footer',  // react组件函数    不显示时设置为undefined
-        //     rowSelection: {},                // checkbox         不显示时设置为undefined
-        //     scroll: { y: 240 }               // 滑动             不显示时设置为undefined
-        // }
         return (
             <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                <BasicCondition conditions={condition} />
-                <BasicOperation className="mt-10 mb-10" operationBtns={operationBtn} />
-                <Table {...state.tableSetting} rowKey={record => record.id} columns={columns} rowSelection={rowSelection} />
+                <Spin spinning={this.state.loading} size="large">
+                    <Calendar
+                        dateCellRender={this.dateCellRender}
+                        monthCellRender={this.monthCellRender}
+                        onPanelChange={this.onPanelChange}
+                        onSelect={this.onSelect}
+                    />
+                </Spin>
             </div>
         )
     }
 }
-
-export default Contract
+export default checkwork
