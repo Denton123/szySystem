b7e631bc777da076089593cad472af47682d3afa
wuyanping <1063218918@qq.com>
2017-12-22 16:46:08 +0800

    [mod] 修改 Attendance.js

--- src/routes/Personnel/Attendance.js
+++ src/routes/Personnel/Attendance.js
@@ -25,64 +25,45 @@ class checkwork extends Component {
             calendarTime: moment().format('YYYY-MM-DD'),
             loading: true
         }, () => {
-            var data = this.state.calendarTime
-            axios.get(`/user/cs?data=${data}`)
-                .then(res => {
-                    console.log('access_token ---- ')
-                    console.log(res)
-                    data = []
-                    if (res.data !== []) {
-                        res.data.forEach((arrItem) => {
-                            data = [...arrItem, ...data]
-                        })
-                    }
-                    this.setState({
-                        attendanceData: data,
-                        loading: false
-                    })
-                })
-                .catch(err => {
-                    console.log(err)
-                })
+            this.requestData()
         })
-        var time = moment()
-        // console.log('time: ' + time)
-        // console.log('date: ' + moment(time)['date']())
-        // console.log('toArray: ' + moment(time).toArray())
-        // console.log('toJSON: ' + moment(time).toJSON())
-        // console.log('toISOString: ' + moment(time).toISOString())
-        // console.log(moment(time).toObject())
-        // console.log('toString: ' + moment(time).toString())
-        // console.log('weeksInYear: ' + moment(time).weeksInYear())
-        // console.log('isoWeeksInYear: ' + moment(time).isoWeeksInYear())
-        // console.log('month: ' + moment(time).startOf('month').format("YYYY-MM-DD"))
-        // console.log('month: ' + moment(time).endOf('month').format("YYYY-MM-DD"))
-        // console.log('day: ' + moment(time).startOf('month').day())
-        // var to=moment(time).endOf('month').format("YYYY-MM-DD")+" 23:59:59"
     }
     getListData = (value) => {
         let oneDayArr = []
         this.state.attendanceData.forEach(item => {
             if (moment(moment(value).format('YYYY-MM-DD')).isSame(moment(item.userCheckTime).format('YYYY-MM-DD'))) {
-                let tip = {type: '', content: ''}
-                switch (item.timeResult) {
-                    case 'Normal':
-                        tip = {type: 'success', content: '正常'}
+                let tip = {type: '', content: '', checkType: ''}
+                switch (item.checkType) {
+                    case 'OnDuty':
+                        tip.checkType = '上班'
                         break
-                    case 'Early':
-                        tip = {type: 'warning', content: '早退'}
-                        break
-                    case 'Late':
-                        tip = {type: 'warning', content: '迟到'}
-                        break
-                    case 'SeriousLate':
-                        tip = {type: 'warning', content: '严重迟到'}
-                        break
-                    case 'NotSigned':
-                        tip = {type: 'warning', content: '未打卡'}
+                    case 'OffDuty':
+                        tip.checkType = '下班'
                         break
                 }
-                oneDayArr.push({ type: tip.type, content: `${moment(item.userCheckTime).format('YY-MM-DD, h:mm')}${tip.content}` })
+                switch (item.timeResult) {
+                    case 'Normal':
+                        tip.type = 'success'
+                        tip.content = '正常'
+                        break
+                    case 'Early':
+                        tip.type = 'warning'
+                        tip.content = '早退'
+                        break
+                    case 'Late':
+                        tip.type = 'warning'
+                        tip.content = '迟到'
+                        break
+                    case 'SeriousLate':
+                        tip.type = 'warning'
+                        tip.content = '严重迟到'
+                        break
+                    case 'NotSigned':
+                        tip.type = 'warning'
+                        tip.content = '未打卡'
+                        break
+                }
+                oneDayArr.push({ type: tip.type, content: `${tip.checkType} ${moment(item.userCheckTime).format('h:mm')} ${tip.content}` })
             }
         })
         return oneDayArr
@@ -95,7 +76,7 @@ class checkwork extends Component {
             <ul className="events">
                 {
                     listData.map(item => (
-                        <li key={item.content}>
+                        <li key={item.content} style={{'listStyleType': 'none'}}>
                             <Badge status={item.type} text={item.content} />
                         </li>
                     ))
@@ -127,29 +108,39 @@ class checkwork extends Component {
             calendarTime: moment(value).format('YYYY-MM-DD'),
             loading: true
         }, () => {
-            var data = this.state.calendarTime
-            axios.get(`/user/cs?data=${data}`)
-                .then(res => {
-                    data = []
-                    if (res.data !== []) {
-                        res.data.forEach((arrItem) => {
-                            data = [...arrItem, ...data]
-                        })
-                    }
-                    this.setState({
-                        attendanceData: data,
-                        loading: false
-                    })
-                })
-                .catch(err => {
-                    console.log(err)
-                })
+            this.requestData()
         })
-        console.log('value: ' + moment(value).format('YYYY-MM-DD'))
     }
 
-    onSelect = () => {
-        console.log(22)
+    onSelect = (value) => {
+        this.setState({
+            calendarTime: moment(value).format('YYYY-MM-DD'),
+            loading: true
+        }, () => {
+            this.requestData()
+        })
+    }
+
+    requestData = () => {
+        var time = this.state.calendarTime
+        axios.get(`/user/cs?time=${time}`)
+            .then(res => {
+                console.log('access_token ---- ')
+                console.log(res)
+                let data = []
+                if (res.data !== []) {
+                    res.data.forEach((arrItem) => {
+                        data = [...arrItem, ...data]
+                    })
+                }
+                this.setState({
+                    attendanceData: data,
+                    loading: false
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }
     render() {
         const child = this.props.child
