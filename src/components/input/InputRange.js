d985bd6777dd2e6aecaa2c88e6a0692c81b54317
wuyanping <1063218918@qq.com>
2018-01-24 17:57:04 +0800

    [mod] 修改 input/InputRange.js

--- src/components/input/InputRange.js
+++ src/components/input/InputRange.js
@@ -34,9 +34,9 @@ class InputRange extends React.Component {
     render() {
         return (
             <InputGroup className="mb-10" compact style={{ width: 250, marginRight: 50 }} >
-                <InputNumber value={this.props.value ? this.props.value.number1 : 0.00} onChange={this.handleNumberTarget('number1')} style={{ width: 100, textAlign: 'center' }} precision={2} placeholder="Minimum" />
+                <InputNumber value={this.props.value ? this.props.value.number1 : 0.00} min={0} onChange={this.handleNumberTarget('number1')} style={{ width: 100, textAlign: 'center' }} precision={2} placeholder="Minimum" />
                 <Input style={{ width: 24, borderLeft: 0, pointerEvents: 'none', backgroundColor: '#fff' }} placeholder="~" disabled />
-                <InputNumber value={this.props.value ? this.props.value.number2 : 0.00} onChange={this.handleNumberTarget('number2')} style={{ width: 100, textAlign: 'center', borderLeft: 0 }} precision={2} placeholder="Maximum" />
+                <InputNumber value={this.props.value ? this.props.value.number2 : 0.00} min={0} onChange={this.handleNumberTarget('number2')} style={{ width: 100, textAlign: 'center', borderLeft: 0 }} precision={2} placeholder="Maximum" />
             </InputGroup>
         )
     }
