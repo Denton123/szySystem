023579c0781b84eddd4d9d8b3a54519c4583391b
wuyanping <1063218918@qq.com>
2018-03-20 14:45:16 +0800

    [mod] 编辑WorkLog

--- src/mobile/routes/Personal/WorkLog.js
+++ src/mobile/routes/Personal/WorkLog.js
@@ -1,13 +1,62 @@
 import React from 'react'
 import {
-    Button
-} from 'antd'
+    List,
+    Card,
+    WingBlank,
+    WhiteSpace,
+    Pagination,
+    Icon,
+    Accordion,
+    Drawer,
+    NavBar
+} from 'antd-mobile'
+
+import './WorkLog.less'
+
+// 引入工具方法
+import {isFunction, isObject, valueToMoment, momentToValue, resetObject, valueToPriceRange, transformValue} from 'UTILS/utils'
+
+import {ajax, index, store, show, update, destroy} from 'UTILS/ajax'
+
+const Item = List.Item
+const Brief = Item.Brief
+const locale = {
+    prevText: 'Prev',
+    nextText: 'Next'
+}
+
+class ListExample extends React.Component {
+    state = {
+    }
+    componentDidMount() {
+    }
+    render() {
+        // 导航栏
+        let navBar = (
+            <NavBar
+                mode="dark"
+                leftContent="menu"
+                rightContent={[
+                    <Icon key="0" type="search" />
+                ]}
+            >WorkLog</NavBar>
+        )
+
+        let
+
+        return (
+            <div>
+                {navBar}
+            </div>
+        )
+    }
+}
 
 class Task extends React.Component {
     render() {
         return (
-            <div style={{ padding: 24, background: '#fff' }}>
-               worklog
+            <div>
+                <ListExample />
             </div>
         )
     }
